5da22f212dc398447114c3b152535bff654eb6a5
Philipp Mark <philipp.mark@student.htldornbirn.at>
2023-12-14 11:28:20 +0100

    Aufgabe 1 Fertig, stunde finished

--- JavaScript 2023-2024/JS 01 Variablen/script.js
+++ JavaScript 2023-2024/JS 01 Variablen/script.js
@@ -1,4 +1,4 @@
-Hier nochmals die Aufgabe aus dem Video: 
+// Hier nochmals die Aufgabe aus dem Video: 
 
 console.log("Hallo"); 
 
@@ -15,26 +15,38 @@ let c = 10;
 // Berechne folgendes und gib das Eregebnis über console.log aus 
 
 // a + b + c 
-console.log("a + b + c "= )
+let result = a + b + c;
+console.log("Das Ergebnis ist " + result);
 
 // (a + b) / c 
-
+let result2 = (a + b) / c;
+console.log("Das Ergebnis ist " + result2);
+ 
 // c -a + b 
+let result3 = c - a + b;
+console.log("Das Ergebnis ist " + result3);
 
 // a + b + c * 3 
+let result4 = a + b + c * 3;
+console.log("Das Ergebnis ist " + result4);
 
 // (a + b + c)/4 
+let result5 = (a + b + c)/4;
+console.log("Das Ergebnis ist " + result5);
 
  
 
 // Ändere die Variablen auf folgende Werte 
 
 // a = 3 
+a = 3;
 
 // b = a + 3 
+b = a + 3;
 
 // c = c -3 
+c = c -3;
+
 
 // gib die neuen Werte der Variablen aus 
-
-
+console.log(a + ", " + b + ", " + c);
