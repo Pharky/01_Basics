5f25377a27c935785e23fe6657f717c8bbc80794
Philipp Mark <philipp.mark@student.htldornbirn.at>
2024-01-11 10:38:06 +0100

    02 JS Strings & Zahlen | Done

---
+++ JavaScript 2023-2024/02 JS Strings u. Zahlen/script.js
@@ -0,0 +1,22 @@
+let a = 7;
+let b = 8;
+
+let ab = a + b;
+
+console.log(ab);
+console.log(ab);
+
+// +, -, /, * 
+
+let c = 7.4;
+let d = 7.5;
+
+result = c + d; 
+console.log(result);
+
+// Texte Strings
+
+let name = "Philipp";
+console.log(name);
+
+console.log("das ergebnis von " + a + " und " + b + " ist " + ab)
