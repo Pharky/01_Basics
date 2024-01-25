608a0f2ce41dabde05697c9cb0e17291b3dab11d
Philipp Mark <philipp.mark@student.htldornbirn.at>
2024-01-25 11:23:21 +0100

    Video Part von aufgabe 05 If-Bedingung

--- JavaScript 2023-2024/05 If-Bedingung/script.js
+++ JavaScript 2023-2024/05 If-Bedingung/script.js
@@ -1,6 +1,13 @@
 
-let number = 17;
+let number = 27;
 
-if (number<20){
-    console.log("Die Zahl ist kleiner als 20" )
+if (number<25){
+    console.log(number);
+} else if (number<50) {
+    console.log("kleiner 50 " + number) 
+} else {
+    console.log("else");
 }
+
+// Es gibt 6 Verschiedene Operatoren, diese sind: < (kleiner als); > (größer als); == (Ist gleich); <= (kleiner oder gleich); >= (größer oder gleich), != (Darf nicht .. sein, ist ungleich)
+
