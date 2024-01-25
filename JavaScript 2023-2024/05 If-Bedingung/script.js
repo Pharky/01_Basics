09ff2a399c53559cfb468b16d5af4efc02d3da27
Philipp Mark <philipp.mark@student.htldornbirn.at>
2024-01-25 11:25:28 +0100

    End of Lesson | 05 If-Bedingung

--- JavaScript 2023-2024/05 If-Bedingung/script.js
+++ JavaScript 2023-2024/05 If-Bedingung/script.js
@@ -11,3 +11,28 @@ if (number<25){
 
 // Es gibt 6 Verschiedene Operatoren, diese sind: < (kleiner als); > (größer als); == (Ist gleich); <= (kleiner oder gleich); >= (größer oder gleich), != (Darf nicht .. sein, ist ungleich)
 
+// erstelle eine Variable number 
+let number2 = 10;
+
+ 
+
+ 
+
+// Aufgabe 1
+// Wenn die Zahl kleiner ist als 20  gib aus "Mini" 
+
+// Wenn die Zahl zw. 20 und 50 ist gib aus "Medium" 
+
+// Wenn die Zahl größer als 50 ist gib aus "Large" 
+
+ 
+
+ 
+
+// Aufgabe 2 
+
+// Wenn die Zahl kleiner gleich 7 ist gib aus "Mini" 
+
+// Wenn die Zahl zwischen 7 und 90 ist gib  aus "Medium" 
+
+// Wenn die Zahl größer als 90 ist gib aus "Large" 
