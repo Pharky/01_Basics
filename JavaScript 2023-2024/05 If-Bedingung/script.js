1357e4a95f2381fb518f526e084c3906e8101040
Philipp Mark <philipp.mark@student.htldornbirn.at>
2024-02-22 11:22:17 +0100

    05 If Bedingung Done

--- JavaScript 2023-2024/05 If-Bedingung/script.js
+++ JavaScript 2023-2024/05 If-Bedingung/script.js
@@ -1,29 +1,32 @@
 
-let number = 27;
+// let number = 27;
 
-if (number<25){
-    console.log(number);
-} else if (number<50) {
-    console.log("kleiner 50 " + number) 
-} else {
-    console.log("else");
-}
+// if (number<25){
+//     console.log(number);
+// } else if (number<50) {
+//     console.log("kleiner 50 " + number) 
+// } else {
+//     console.log("else");
+// }
 
 // Es gibt 6 Verschiedene Operatoren, diese sind: < (kleiner als); > (größer als); == (Ist gleich); <= (kleiner oder gleich); >= (größer oder gleich), != (Darf nicht .. sein, ist ungleich)
 
 // erstelle eine Variable number 
-let number2 = 10;
-
- 
-
- 
+let number2 = 30;
 
 // Aufgabe 1
 // Wenn die Zahl kleiner ist als 20  gib aus "Mini" 
-
+if (number2<20) {
+    console.log("mini");
+}
 // Wenn die Zahl zw. 20 und 50 ist gib aus "Medium" 
-
+if (number2>20 <50) {
+    console.log("Medium")
+}
 // Wenn die Zahl größer als 50 ist gib aus "Large" 
+if (number2>50) {
+    console.log("large")
+}
 
  
 
@@ -32,7 +35,14 @@ let number2 = 10;
 // Aufgabe 2 
 
 // Wenn die Zahl kleiner gleich 7 ist gib aus "Mini" 
-
+if (number2<=7) {
+    console.log("Mini")
+}
 // Wenn die Zahl zwischen 7 und 90 ist gib  aus "Medium" 
-
+if (number2>7 <90) {
+    console.log("Medium")
+}
 // Wenn die Zahl größer als 90 ist gib aus "Large" 
+if (number2>90) {
+    console.log("Large")
+}
