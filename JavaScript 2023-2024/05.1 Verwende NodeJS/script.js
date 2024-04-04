1634fd2effff56eafd75764cb1bb24c187acfb3e
Philipp Mark <philipp.mark@student.htldornbirn.at>
2024-04-04 11:28:52 +0200

    nur 5.1, und mit 6 angefangen, mit node nicht zurechtgekommen

---
+++ JavaScript 2023-2024/05.1 Verwende NodeJS/script.js
@@ -0,0 +1,15 @@
+
+
+console.log("Wilkommen Peter!");
+console.log("Drücke ");
+console.log("1 für Start");
+console.log("2 für Statistik");
+console.log("3 für Ende");
+
+//     cd ..            Einen ordner hoch
+//     cd demo          In den Ordner dem ??
+//     pwd              zeigt in welchem Ordner ich bin
+//     node sciprt.js   führt script.js im aktuellen ordner aus
+//     ls               Zeigt alle Files im aktuellen Directory
+//     cd J (Tab)       Schlägt den passenden Ordner vor
+//     Pfeil oben
